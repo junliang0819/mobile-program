934b47a14861fe5f92e40f9de8e7598939871ad1
陈俊良 <jlchen@cjl.lan>
2017-09-02 17:55:06 +0800

    添加头部met信息a

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+// import Hello from '@/components/Hello'
 import Home from '@/views/home'
 
 Vue.use(Router)
 
 let routes = [
+  // {
+  //   path: '/',
+  //   name: 'Hello',
+  //   component: Hello
+  // },
   {
     path: '/',
-    name: 'Hello',
-    component: Hello
-  },
-  {
-    path: '/home',
     name: 'home',
     component: Home
   }
