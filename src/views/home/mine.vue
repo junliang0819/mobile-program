9a27e367adabb239fe3c0644bd3a85e4c22c36d6
hao.luo <hao.luo>
2017-09-04 18:15:10 +0800

    mine

--- src/views/home/mine.vue
+++ src/views/home/mine.vue
@@ -10,18 +10,64 @@
       </mt-cell>
       <mt-cell title="切换门店" is-link value="测试"></mt-cell>
     </div>
-    <div>
+    <div style="margin-bottom: 15px;">
       <mt-cell is-link>
         <div slot="title">
-          <i class="iconfont icon-dianpu"></i>
+          <i class="iconfont icon-my_fill_light"></i>
           <span>账号设置</span>
         </div>
         <div>18282828282</div>
       </mt-cell>
       <mt-cell is-link>
         <div slot="title">
-          <i class="iconfont icon-dianpu"></i>
+          <i class="iconfont icon-shouyinyuan"></i>
           <span>收银员设置</span>
+        </div>
+      </mt-cell>
+      <mt-cell is-link>
+        <div slot="title">
+          <i class="iconfont icon-shebei"></i>
+          <span>设备管理</span>
+        </div>
+      </mt-cell>
+      <mt-cell is-link>
+        <div slot="title">
+          <i class="iconfont icon-jewelry1"></i>
+          <span>高级服务</span>
+        </div>
+      </mt-cell>
+    </div>
+    <div style="margin-bottom: 15px;">
+      <mt-cell is-link>
+        <div slot="title">
+          <i class="iconfont icon-newshotfill"></i>
+          <span>收款方式</span>
+        </div>
+      </mt-cell>
+      <mt-cell is-link>
+        <div slot="title">
+          <i class="iconfont icon-shezhi"></i>
+          <span>收银设置</span>
+        </div>
+      </mt-cell>
+      <mt-cell is-link>
+        <div slot="title">
+          <i class="iconfont icon-jifen"></i>
+          <span>积分规则</span>
+        </div>
+      </mt-cell>
+    </div>
+    <div>
+      <mt-cell is-link>
+        <div slot="title">
+          <i class="iconfont icon-ziliaoku"></i>
+          <span>收银物料</span>
+        </div>
+      </mt-cell>
+      <mt-cell is-link>
+        <div slot="title">
+          <i class="iconfont icon-like_fill"></i>
+          <span>关于我们</span>
         </div>
       </mt-cell>
     </div>
