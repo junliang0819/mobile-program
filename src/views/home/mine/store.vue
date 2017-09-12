079b20924327eac5dfb36662f15cee24300282c6
hao.luo <hao.luo>
2017-09-12 17:13:53 +0800

    address

--- src/views/home/mine/store.vue
+++ src/views/home/mine/store.vue
@@ -36,14 +36,30 @@
       </mt-cell>
     </div>
     <div class="margin-bt15">
-      <mt-cell is-link>
-        <div slot="title">
+      <mt-cell to="/mine">
+        <div slot="title" class="border-none">
           <span>门店地址</span>
         </div>
       </mt-cell>
       <mt-cell>
-        <span>北京市</span>
+        <div slot="title" class="border-none">
+          <span @click="open" class="address">{{ address }}</span>
+        </div>
       </mt-cell>
+
+      <mt-popup v-model="visible" position="bottom" class="mint-datetime">
+        <mt-picker
+          :slots="addressSlots"
+          @change="onAddressChange"
+          :visible-item-count="5"
+          class="mint-datetime-picker"
+          ref="picker"
+          v-model="value"
+          show-toolbar>
+          <span class="mint-datetime-action mint-datetime-cancel" @click="visible = false;$emit('cancel')">取消</span>
+          <span class="mint-datetime-action mint-datetime-confirm" @click="confirm">确定</span>
+        </mt-picker>
+      </mt-popup>
     </div>
     <div class="margin-bt15">
       <mt-cell>
@@ -69,6 +85,12 @@
 </template>
 
 <style scoped>
+  .border-none {
+    border: none;
+  }
+  .address {
+    color: #888;
+  }
   .margin-bt15 {
     margin-bottom: 15px;
   }
@@ -89,12 +111,71 @@
   }
 </style>
 <script>
+  import s from './address3.json'
   export default {
     data () {
       return {
+        value: null,
+        visible: false,
+        companyName: '',
+        addressSlots: [
+          {
+            flex: 1,
+            defaultIndex: 1,
+            values: Object.keys(s),
+            className: 'slot1',
+            textAlign: 'center'
+          }, {
+            divider: true,
+            content: '-',
+            className: 'slot2'
+          }, {
+            flex: 1,
+            values: [],
+            className: 'slot3',
+            textAlign: 'center'
+          }, {
+            divider: true,
+            content: '-',
+            className: 'slot4'
+          }, {
+            flex: 1,
+            values: [],
+            className: 'slot5',
+            textAlign: 'center'
+          }
+        ],
+        address: '北京市 市辖区 东城区',
+        addressProvince: '省',
+        addressCity: '市',
+        addressXian: '区'
       }
     },
     methods: {
+      open () {
+        this.visible = true
+      },
+      confirm () {
+        this.visible = false
+        this.address = this.addressProvince + ' ' + this.addressCity + ' ' + this.addressXian
+      },
+      onAddressChange (picker, values) {
+        let shi = Object.keys(s[values[0]])
+        let index = shi.indexOf(values[1])
+        let xian = s[values[0]][shi[index]]
+        picker.setSlotValues(1, shi)
+        this.addressProvince = values[0]
+        this.addressCity = values[1]
+        this.addressXian = values[2]
+        picker.setSlotValues(2, xian)
+      }
+    },
+    mounted () {
+      this.$nextTick(() => {
+        setTimeout(() => {
+          this.addressSlots[0].defaultIndex = 0
+        }, 100)
+      })
     }
   }
 </script>
