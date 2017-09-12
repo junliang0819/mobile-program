41f15886c5a3cc9bd442de7f25f8b9776bff9b5a
陈俊良 <jlchen@192.168.0.180>
2017-09-12 16:12:38 +0800

    商品管理

--- src/views/home/cash/goods.vue
+++ src/views/home/cash/goods.vue
@@ -5,13 +5,13 @@
         <ul>
           <li :class="currentIndex==index?'active':''" v-for="(item,index) in types" @click="changeType(item.id,index)">
             {{item.name}}
-            <mt-badge size="small">30</mt-badge>
+            <mt-badge v-show="currentIndex==index&&item.chosen>=1" size="small">{{item.chosen}}</mt-badge>
           </li>
         </ul>
       </div>
       <div class="right">
         <ul>
-          <li v-for="(good,index) in currentGoods" class="ks-clear" @click="chooseGoods(index)">
+          <li v-for="(good,index) in types[currentIndex].goods" class="ks-clear" @click="chooseGoods(index)">
             <img :src="good.pic" alt="" width="50" height="50">
             <div>
               <div>
@@ -19,42 +19,48 @@
               </div>
               ￥ {{good.price}}/千克
             </div>
-            <i v-show="editable" :class="good.choosen?'iconfont icon-rechargefill':'iconfont icon-recharge'"></i>
+            <i v-show="editable" :class="good.chosen?'iconfont icon-rechargefill':'iconfont icon-recharge'"></i>
           </li>
         </ul>
       </div>
     </div>
-    <mt-tabbar v-model="selected" class="is-fixed">
-      <mt-tab-item id="cash" @click.native="editable = true">
-        批量编辑
-      </mt-tab-item>
-      <mt-tab-item id="state">
-        新增商品
-      </mt-tab-item>
-    </mt-tabbar>
+    <div class="fix-bottom">
+      <span @click="handleLeft">
+        {{leftButton}}
+      </span>
+      <span @click="handleRight">
+        {{rightButton}}
+      </span>
+    </div>
   </div>
 </template>
 <style scoped>
   .box {
     display: flex;
     justify-content: flex-start;
+    position: absolute;
+    left: 0;
+    right: 0;
+    top: 0;
+    bottom: 30px;
   }
   .box .left {
     background: grey;
 
   }
   .box .left li{
-    padding: 10px;
+    padding: 26px;
   }
   .box .left li.active{
     background: #fff;
     color: #dfa900;
   }
   .box .right {
-
+    flex: 1;
   }
   .box .right li {
-
+    border-bottom: 1px solid grey;
+    padding: 10px;
   }
   .box .right li>img {
     float: left;
@@ -65,6 +71,17 @@
   }
   .box .right li>div>div {
     color: #000;
+  }
+  .fix-bottom {
+    position: fixed;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    text-align: center;
+  }
+  .fix-bottom span {
+    float: left;
+    width: 50%;
   }
 </style>
 
@@ -79,23 +96,50 @@ export default {
       selected: '',
       currentIndex: 0,
       editable: false,
+      leftButton: '批量编辑',
+      rightButton: '新增商品',
       types: [
         {
           id: 1,
-          name: '商品分类1'
+          name: '商品分类1',
+          chosen: 0,
+          goods: [
+            {
+              id: 1,
+              pic: 'http://www.cnblogs.com/skins/summerGarden/images/header.jpg',
+              name: '测试商品1', 
+              price: '10.00',
+              chosen: false
+            },
+            {
+              id: 2,
+              pic: 'http://www.cnblogs.com/skins/summerGarden/images/header.jpg',
+              name: '测试商品2',
+              price: '10.00',
+              chosen: false
+            }
+          ]
         },
         {
           id: 2,
-          name: '商品分类2'
-        }
-      ],
-      currentGoods: [
-        {
-          id: 1,
-          pic: 'http://www.cnblogs.com/skins/summerGarden/images/header.jpg',
-          name: '测试商品1',
-          price: '10.00',
-          choosen: false
+          name: '商品分类2',
+          chosen: 0,
+          goods: [
+            {
+              id: 1,
+              pic: 'http://www.cnblogs.com/skins/summerGarden/images/header.jpg',
+              name: '测试商品3',
+              price: '10.00',
+              chosen: false
+            },
+            {
+              id: 2,
+              pic: 'http://www.cnblogs.com/skins/summerGarden/images/header.jpg',
+              name: '测试商品4',
+              price: '10.00',
+              chosen: false
+            }
+          ]
         }
       ]
     }
@@ -103,22 +147,37 @@ export default {
   methods: {
     changeType (id, index) {
       this.currentIndex = index
-      this.currentGoods = [
-        {
-          id: 2,
-          pic: 'xxx',
-          name: '测试商品2',
-          price: '10.00',
-          choosen: false
-        }
-      ]
+      
     },
     chooseGoods (index) {
       if (this.editable) {
-        this.currentGoods[index].choosen = !this.currentGoods[index].choosen
+        let isChosen = this.types[this.currentIndex].goods[index].chosen
+        if (!isChosen) {
+          this.types[this.currentIndex].chosen = this.types[this.currentIndex].chosen + 1
+        } else {
+          this.types[this.currentIndex].chosen = this.types[this.currentIndex].chosen - 1
+        }
+        this.types[this.currentIndex].goods[index].chosen = !this.types[this.currentIndex].goods[index].chosen
       } else {
         this.$router.push('./detail')
       }
+    },
+    handleLeft () {
+      if (this.editable) {
+        this.types = this.types.map(el => {
+          el.chosen = 0
+          return el
+        })
+        this.leftButton = '批量编辑'
+        this.rightButton = '新增商品'
+      } else {
+        this.leftButton = '取消'
+        this.rightButton = '删除'
+      }
+      this.editable = !this.editable
+    },
+    handleRight () {
+
     }
   }
 
